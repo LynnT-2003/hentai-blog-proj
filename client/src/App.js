65e545d2b0594e25a2a904097214c0e6f02a63ec
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-06-11 17:23:36 +0700

    Created added route for creating a new post

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import CreatePost from "./pages/CreatePost";
 import { UserContextProvider } from "./UserContext";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path={"/login"} element={<LoginPage />} />
           <Route path={"/register"} element={<RegisterPage />} />
+          <Route path={"/create"} element={<CreatePost />} />
         </Route>
       </Routes>
     </UserContextProvider>
---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,3 @@
+export default function CreatePost() {
+  return <div>here you create your new post</div>;
+}
